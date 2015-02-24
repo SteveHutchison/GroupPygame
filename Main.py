5d66d83e5e6bdef8c3fae50a873eac81aafb8957
unknown <1301955@MC8183.uad.ac.uk>
2015-02-24 16:27:41 +0000

    CHANGED EVERYTHING!

--- Main.py
+++ Main.py
@@ -1,24 +1,29 @@
-import sys, pygame
-import random
+import pygame, sys, random
 from pygame.locals import *
 
+# set up pygame
 pygame.init()
 mainClock = pygame.time.Clock()
 
-player = pygame.image.load("M:/groupPy/img/player.png")
+player_image = pygame.image.load("M:/groupPy/img/player.png")
 background = pygame.image.load("M:/groupPy/img/background.png")
 enemy_image = pygame.image.load("M:/groupPy/img/enemy_1.png")
 
-playerRect = player.get_rect()
 backgroundRect = background.get_rect()
 
-size = (width, height) = background.get_size()
-screen = pygame.display.set_mode(size)
+# set up the window
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 800
+screen = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Input')
 
-b3x = 200
-b3y = 750
-movex = 6
-movey = 0
+# set up the colors
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+RANDOMCOLOUR = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+WHITE = (255, 255, 255)
 
 enemyCounter = 0
 NEWENEMY = 8
@@ -26,69 +31,80 @@ ENEMYSIZE = 50
 ENEMYSPEED = 6
 enemies = []
 
-left = False
-right = False
+player = pygame.Rect(300, 700, 32, 32)
 
-while 1:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		
-		if event.type == pygame.KEYDOWN:
-			if event.key == K_LEFT:
-				left = True
-			if event.key == K_RIGHT:
-				right = True
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False
+
+MOVESPEED = 8
+
+while True:
+    # check for events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            # change the keyboard variables
+            if event.key == K_LEFT or event.key == ord('a'):
+                moveRight = False
+                moveLeft = True
+            if event.key == K_RIGHT or event.key == ord('d'):
+                moveLeft = False
+                moveRight = True
+            if event.key == K_UP or event.key == ord('w'):
+                moveDown = False
+                moveUp = True
+            if event.key == K_DOWN or event.key == ord('s'):
+                moveUp = False
+                moveDown = True
+        if event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            if event.key == K_LEFT or event.key == ord('a'):
+                moveLeft = False
+            if event.key == K_RIGHT or event.key == ord('d'):
+                moveRight = False
+            if event.key == K_UP or event.key == ord('w'):
+                moveUp = False
+            if event.key == K_DOWN or event.key == ord('s'):
+                moveDown = False
 				
-		if event.type == pygame.KEYUP:
-			if event.key==pygame.K_LEFT:
-				left = False
-			if event.key==pygame.K_RIGHT:
-				right = False
-	
-	if b3x + movex < 0:
-		left = False
-		
-	if b3x + playerRect.width + movex > backgroundRect.width:
-		right = False
-	
-	if right == True:
-		b3x += movex
-		
-	if left == True:
-		b3x -= movex
-		
-	enemyCounter += 1
-	if enemyCounter >= NEWENEMY:
-		# add new enemy
-		#newEnemySpawn = {'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-ENEMYSIZE), 0 - ENEMYSIZE, ENEMYSIZE, ENEMYSIZE),
-        #            'speed': random.randint(ENEMYSPEEDMIN, ENEMYSPEEDMAX),
-        #            }
+    enemyCounter += 1
+    if enemyCounter >= NEWENEMY:
 					
-		enemies.append({'rect': pygame.Rect(random.randint(0, backgroundRect.width-ENEMYSIZE), 0 - ENEMYSIZE, ENEMYSIZE, ENEMYSIZE),
+        enemies.append({'rect': pygame.Rect(random.randint(0, WINDOWWIDTH-ENEMYSIZE), 0 - ENEMYSIZE, ENEMYSIZE, ENEMYSIZE),
                     'speed': ENEMYSPEED,
-                    'surface':pygame.transform.scale(enemy_image, (ENEMYSIZE, ENEMYSIZE))
+					'surface':pygame.transform.scale(enemy_image, (ENEMYSIZE, ENEMYSIZE))
                     })
-		enemyCounter = 0
-		
+        enemyCounter = 0
+	# draw the black background onto the surface
+    screen.blit(background, backgroundRect)		
+    # move the player
+    if moveDown and player.bottom < WINDOWHEIGHT:
+        player.top += MOVESPEED
+    if moveUp and player.top > 0:
+        player.top -= MOVESPEED
+    if moveLeft and player.left > 0:
+        player.left -= MOVESPEED
+    if moveRight and player.right < WINDOWWIDTH:
+        player.right += MOVESPEED
 	# move the enemies
-	for b in enemies:
+    for b in enemies:
 		b['rect'].move_ip(0, ENEMYSPEED)
 	# check for collisions
-	#for b in enemies:
-		#if playerRect.colliderect(b['rect']):
-			#pygame.quit()
-			#sys.exit()
-			#print "collision"
-	
-	# Draw the Background
-	screen.blit(background, backgroundRect)
-	
-	# Draw the enemies
-	for b in enemies:
-		screen.blit(enemy_image, b['rect'])
-		
-	# Draw the Player
-	screen.blit(player,(b3x,b3y))
-	
-	pygame.display.flip()
-	mainClock.tick(40)
+    for b in enemies:
+        if player.colliderect(b['rect']):
+			pygame.quit()
+			sys.exit() 
+    # draw the player onto the surface
+    screen.blit(player_image,(player))
+	# draw the enemies
+    for b in enemies:
+        screen.blit(b['surface'], b['rect'])
+	# draw the window onto the screen
+    pygame.display.update()
+    mainClock.tick(40)
