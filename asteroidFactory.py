1e6557725d1f487673d9794ab369a1bdbaf4a5d9
unknown <1400990@MC8184.uad.ac.uk>
2015-02-26 12:39:36 +0000

    Merge branch 'audio' of https://github.com/SteveHutchison/GroupPygame.git
    Conflicts:
    	Main.py
    	fontRenderer.pyc

---
+++ asteroidFactory.py
@@ -0,0 +1,69 @@
+# asteroidFactory
+import pygame, random
+
+class AsteroidFactory:
+
+	def __init__(self, file_path):
+		## variables declared here are unique to each instance
+
+		self.image = pygame.image.load(file_path)
+
+		self.SIZE = 50
+		self.MAX_SPEED = 5
+		self.MIN_SPEED = 2
+
+		self.counter = 0
+
+		# whenever counter => spawnrate a new asteroid is made
+		self.frameBetweenSpawns = 40
+
+		self.asteroids = []
+
+		# how many points to give the player when an asteroid
+		# leaves the screen
+		self.reward = 10
+		# how much to hurt the player if they hit the asteroid
+		self.damage = 20
+
+	def spawn(self, width):
+		self.counter += 1
+
+		if self.counter >= self.frameBetweenSpawns:
+			self.asteroids.append({'rect': pygame.Rect(random.randint(0, width - self.SIZE), 0 - self.SIZE, self.SIZE, self.SIZE),
+						'speed': random.randint(self.MIN_SPEED, self.MAX_SPEED),
+						'surface':pygame.transform.scale(self.image, (self.SIZE, self.SIZE))
+						})
+			self.counter = 0
+
+
+	def remove(self, score, height):
+			for a in self.asteroids[:]:
+				if a['rect'].top > height:
+					self.asteroids.remove(a)
+					score += self.reward
+
+	def move(self):
+		for a in self.asteroids:
+			a['rect'].move_ip(0, a['speed'])
+
+	def draw(self, target):
+		for a in self.asteroids:
+			target.blit(a['surface'], a['rect'])
+
+	def remove_if_touching(self, bullets):
+		for a in self.asteroids:
+			for b in bullets:
+				if (b['rect']).colliderect(a['rect']):
+					bullets.remove(b)
+
+	def collide_and_hurt(self, playerRect, playerHealth):
+		for a in self.asteroids:
+			if playerRect.colliderect(a['rect']):
+				self.asteroids.remove(a)
+				return playerHealth - self.damage
+		return playerHealth
+
+	def remove_all(self):
+		for a in self.asteroids:
+			self.asteroids.remove(a)
+		asteroids = []
